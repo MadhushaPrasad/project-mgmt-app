b8cd6b4bb926af11294e48adcfe00975f535a13d
Madhusha Prasad <madushaprasad21@gmail.com>
2025-04-28 19:06:42 +0530

    chore: create project type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,9 +6,20 @@ const ClientType = new GraphQLObjectType({
   name: 'Client',
   fields: () => ({
     id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    phone: {type: GraphQLString},
+  }),
+});
+
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: {type: GraphQLString},
     clientId: {type: GraphQLString},
     name: {type: GraphQLString},
     description: {type: GraphQLString},
     status: {type: GraphQLString},
   }),
-});
+})
