e67d97afcac03edf0a64fbbffdccdd98abca8121
Madhusha Prasad <madushaprasad21@gmail.com>
2025-04-28 19:04:58 +0530

    chore: create client type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,14 @@
 const { clients, projects} = require('../sampleData');
 
 const {GraphQLObjectType} = require('graphql');
+
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: {type: GraphQLString},
+    clientId: {type: GraphQLString},
+    name: {type: GraphQLString},
+    description: {type: GraphQLString},
+    status: {type: GraphQLString},
+  }),
+});
